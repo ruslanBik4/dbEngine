ed4943fc56d8d6bb93b838d0582fdfcb5754d24f
Руслан Бикчентаев <BIk4ruslan@gmail.com>
2026-01-01 13:13:54 +0200

    [MOD] go 1.25

--- dbEngine/psql/log_pgx.go
+++ dbEngine/psql/log_pgx.go
@@ -23,57 +23,61 @@ type pgxLog struct {
 
 func (l *pgxLog) Log(ctx context.Context, ll pgx.LogLevel, msg string, data map[string]any) {
 	sql, hasSQL := data["sql"].(string)
-	err, isPgErr := data["err"].(*pgconn.PgError)
-	if !hasSQL && isPgErr {
-		sql = err.Where
-	}
-	if len(sql) > 255 {
-		sql = sql[:255]
-	}
-	if isPgErr {
-		sql += fmt.Sprintf("(%s) %s %s:%d", err.Hint, err.Where, err.File, err.Line)
-	}
-
-	switch ll {
-	case pgx.LogLevelTrace, pgx.LogLevelDebug:
-		logs.DebugLog("[[PGX]] %s %+v", msg, data)
-	case pgx.LogLevelInfo:
-		logs.StatusLog("[[PGX]] %s %+v", msg, data)
-	case pgx.LogLevelWarn:
+	_, isPgErr := data["err"].(*pgconn.PgError)
+	switch err := data["err"].(type) {
+	//case *pgconn.connectError:
+	case *pgconn.PgError:
+		if !hasSQL && isPgErr {
+			sql = err.Where
+		}
+		if len(sql) > 255 {
+			sql = sql[:255]
+		}
 		if isPgErr {
-			logs.ErrorLog(err, msg, sql, data["args"])
-		} else {
-			logs.DebugLog(msg, data)
+			sql += fmt.Sprintf("(%s) %s %s:%d", err.Hint, err.Where, err.File, err.Line)
 		}
 
-	case pgx.LogLevelError:
-		if isPgErr {
-			if dbEngine.IsErrorAlreadyExists(err) {
-				submatch := dbEngine.RegAlreadyExists.FindStringSubmatch(err.Error())
-				fileName := submatch[2] + ".ddl"
-				//switch submatch[0] {
-				//case "role":
-				//	fileName = err.
-				//}
-				logs.CustomLog(logs.WARNING, "ALREADY_EXISTS", fileName, int(err.Line), err.Message, logs.FgInfo)
+		switch ll {
+		case pgx.LogLevelTrace, pgx.LogLevelDebug:
+			logs.DebugLog("[[PGX]] %s %+v", msg, data)
+		case pgx.LogLevelInfo:
+			logs.StatusLog("[[PGX]] %s %+v", msg, data)
+		case pgx.LogLevelWarn:
+			if isPgErr {
+				logs.ErrorLog(err, msg, sql, data["args"])
 			} else {
-				logs.ErrorLog(err, "%s: %s, '%s', args: %+v", msg, err.Detail, sql, data["args"])
+				logs.DebugLog(msg, data)
 			}
 
-			l.pool.addNotice(data["pid"].(uint32), (*pgconn.Notice)(err))
-		} else if err, ok := data["err"].(error); ok {
-			logs.ErrorLog(err, msg, data)
-		} else {
-			logs.DebugLog(msg, data)
-		}
+		case pgx.LogLevelError:
+			if isPgErr {
+				if dbEngine.IsErrorAlreadyExists(err) {
+					submatch := dbEngine.RegAlreadyExists.FindStringSubmatch(err.Error())
+					fileName := submatch[2] + ".ddl"
+					//switch submatch[0] {
+					//case "role":
+					//	fileName = err.
+					//}
+					logs.CustomLog(logs.WARNING, "ALREADY_EXISTS", fileName, int(err.Line), err.Message, logs.FgInfo)
+				} else {
+					logs.ErrorLog(err, "%s: %s, '%s', args: %+v", msg, err.Detail, sql, data["args"])
+				}
 
-	case pgx.LogLevelNone:
-		if ch, ok := ctx.Value("debugChan").(chan any); ok {
-			ch <- data
-		}
+				l.pool.addNotice(data["pid"].(uint32), (*pgconn.Notice)(err))
+			} else if err, ok := data["err"].(error); ok {
+				logs.ErrorLog(err, msg, data)
+			} else {
+				logs.DebugLog(msg, data)
+			}
 
-	default:
-		logs.ErrorLog(errors.New("invalid level "), ll.String())
+		case pgx.LogLevelNone:
+			if ch, ok := ctx.Value("debugChan").(chan any); ok {
+				ch <- data
+			}
+
+		default:
+			logs.ErrorLog(errors.New("invalid level "), ll.String())
+		}
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/ruslanBik4/dbEngine
 
-go 1.24
-
-toolchain go1.24.0
+go 1.25
 
 require (
 	github.com/go-errors/errors v1.5.1
@@ -13,29 +11,28 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/ruslanBik4/gotools v0.0.3
 	github.com/ruslanBik4/logs v0.0.131
-	github.com/stretchr/testify v1.10.0
-	github.com/valyala/fasthttp v1.59.0
+	github.com/stretchr/testify v1.11.1
+	github.com/valyala/fasthttp v1.68.0
 	github.com/valyala/quicktemplate v1.8.0
-	golang.org/x/net v0.37.0
+	golang.org/x/net v0.48.0
 	gopkg.in/yaml.v3 v3.0.1
-
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/getsentry/sentry-go v0.31.1 // indirect
+	github.com/getsentry/sentry-go v0.40.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/objx v0.5.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
 )
